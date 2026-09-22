737f33fa3252cd6c456ae1c995b600c13f1c9f64
Yashshelke9353 <157669106+Yashshelke9353@users.noreply.github.com>
2026-09-22 13:42:54 +0530

    Fix: use CompressedStaticFilesStorage, pin Python 3.12

--- tiktaktoe_project/settings.py
+++ tiktaktoe_project/settings.py
@@ -76,7 +76,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "app" / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "app.User"
